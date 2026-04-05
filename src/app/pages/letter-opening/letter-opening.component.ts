54862dd66aae7ee79811aab0600358813baffa8e
Atharv <atharvastaputre5@gmail.com>
2026-04-05 12:23:33 +0530

    update wedding invite changes

--- src/app/pages/letter-opening/letter-opening.component.ts
+++ src/app/pages/letter-opening/letter-opening.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,12 +6,19 @@ import { Router } from '@angular/router';
   templateUrl: './letter-opening.component.html',
   styleUrls: ['./letter-opening.component.scss']
 })
-export class LetterOpeningComponent {
+export class LetterOpeningComponent implements OnInit   {
 
   isOpening = false;
 
   constructor(private router: Router) {}
 
+   ngOnInit() {
+    // Auto open after 5 seconds if not clicked
+    setTimeout(() => {
+      this.openEnvelope();
+    }, 5000);
+  }
+  
   openEnvelope() {
     // Prevent multiple clicks
     if (this.isOpening) return;
